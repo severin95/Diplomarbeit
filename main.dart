20aedace3d2e2580fe7a26a742e3f7a15b3b6dab
severin95 <111231394+severin95@users.noreply.github.com>
2022-08-15 21:55:05 +0200

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,43 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:login_bildschirm/application/auth/authbloc/auth_bloc.dart';
-import 'package:login_bildschirm/injection.dart' as di;
-import 'package:login_bildschirm/presentation/routes/router.gr.dart' as r;
-import 'package:login_bildschirm/presentation/signup/signup_page.dart';
-import 'package:login_bildschirm/theme.dart';
-
-import 'injection.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await di.init();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final _appRouter = r.AppRouter();
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => sl<AuthBloc>(),
-        )
-      ],
-      child: MaterialApp.router(
-        routeInformationParser: _appRouter.defaultRouteParser(),
-        routerDelegate: _appRouter.delegate(),
-        debugShowCheckedModeBanner: false,
-        title: 'Login Bildschirm',
-        /* theme: AppTheme.lightTheme,*/
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.dark,
-      ),
-    );
-  }
-}
-
-class Authbloc {}
+Test
+  Test
+  Test
+  TestTest
+  
+  
+  Test
+  
+  TestTest
+  
+  Test
